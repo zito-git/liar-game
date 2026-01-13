764c7d492e10a16892113c6387a3e668687289bd
지토지토 <iam@zito.kr>
2026-01-14 08:03:27 +0900

    add: ConfigModule.forRoot()

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GameModule } from './game/game.module';
 import { RedisModule } from './redis/redis.module';
+import { ConfigModule } from '@nestjs/config';
+import { CreateModule } from './create/create.module';
 
 @Module({
-  imports: [GameModule, RedisModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), GameModule, RedisModule, CreateModule],
   controllers: [AppController],
   providers: [AppService],
 })
