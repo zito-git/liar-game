0e997f406b6d059a411f2d8f175c7762493ed17f
지토지토 <iam@zito.kr>
2026-01-14 20:11:07 +0900

    add: redis service

--- backend/src/redis/redis.service.ts
+++ backend/src/redis/redis.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import Redis from 'ioredis';
 
 @Injectable()
 export class RedisService {
-  private readonly client: Redis;
+  readonly client: Redis;
 
-  constructor() {
-    this.client = new Redis({ host: 'localhost', port: 6379 });
+  constructor(private readonly config: ConfigService) {
+    this.client = new Redis({
+      host: config.get<string>('REDIS_SERVER'),
+      port: config.get('REDIS_PORT'),
+    });
   }
 }
