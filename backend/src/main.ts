fd2df003298ea972889a8f4822c1a0f304101206
지토지토 <iam@zito.kr>
2026-01-18 01:33:51 +0900

    feat: GlobalFilter 사용

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { GlobalExceptionFilter } from './filter/global.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new GlobalExceptionFilter());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
