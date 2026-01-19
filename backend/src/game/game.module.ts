4a4a46ecd9b8c22eae0ec5d69f908f5ce5765831
지토지토 <iam@zito.kr>
2026-01-19 13:56:20 +0900

    feat: CustomJwt모듈

--- backend/src/game/game.module.ts
+++ backend/src/game/game.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { GameGateway } from './game.gateway';
+import { CustomJwtModule } from 'src/jwt/custom-jwt.module';
 
 @Module({
+  imports: [CustomJwtModule],
   providers: [GameGateway],
 })
 export class GameModule {}
