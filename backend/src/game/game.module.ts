4b4f7bd85d4acb047ca746e75b3d02b6b50748fe
지토지토 <iam@zito.kr>
2026-01-19 10:55:26 +0900

    feat: Redis모듈 추가

--- backend/src/game/game.module.ts
+++ backend/src/game/game.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { GameGateway } from './game.gateway';
-import { RedisModule } from 'src/redis/redis.module';
 
 @Module({
-  imports: [RedisModule],
   providers: [GameGateway],
 })
 export class GameModule {}
