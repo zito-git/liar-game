be94f845aceac7dcb3225ac5d8b9c9b095195618
지토지토 <iam@zito.kr>
2026-01-19 10:55:10 +0900

    feat: 소켓 접속 시, 테스트용으로 cors 전체 허용

--- backend/src/game/game.gateway.ts
+++ backend/src/game/game.gateway.ts
@@ -1,10 +1,13 @@
 import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
-import { RedisService } from 'src/redis/redis.service';
 
-@WebSocketGateway(80, { namespace: 'game', transports: ['websocket'] })
+@WebSocketGateway(80, {
+  namespace: 'game',
+  transports: ['websocket'],
+  cors: {
+    origin: '*',
+  },
+})
 export class GameGateway {
-  constructor(private readonly redis: RedisService) {}
-
   @SubscribeMessage('message')
   handleMessage(client: any, payload: any): string {
     return 'Hello world!';
