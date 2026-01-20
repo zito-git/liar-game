683e750cf44058fc94362e5eb8e343f2791f0609
지토지토 <iam@zito.kr>
2026-01-20 12:14:25 +0900

    feat: 방 접속

--- backend/src/game/game.gateway.ts
+++ backend/src/game/game.gateway.ts
@@ -1,4 +1,10 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server } from 'socket.io';
+import { CustomJwtService } from 'src/jwt/custom-jwt.service';
 
 @WebSocketGateway(80, {
   namespace: 'game',
@@ -8,8 +14,43 @@ import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
   },
 })
 export class GameGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+  constructor(private readonly jwtService: CustomJwtService) {}
+  @WebSocketServer()
+  server: Server;
+
+  @SubscribeMessage('join')
+  async handleJoinRoom(client: any, payload: any) {
+    const jwt: string = client.handshake.headers.authorization;
+    const roomId: string = payload.roomId;
+
+    //jwt 검증
+    const tokenResult = await this.jwtService.verifyJwt(jwt);
+    if (!tokenResult) {
+      return 'Error';
+    }
+
+    // client 상태 저장
+    client.data.roomId = roomId;
+    client.data.nickname = tokenResult.nickname;
+
+    //방 접속하기
+    client.join(roomId);
+
+    console.log(client.id);
+    //본인에게만
+    client.emit('join_success', {
+      roomId,
+      nickname: tokenResult.nickname,
+      socketId: client.id,
+    });
+
+    //방 다른 사람들에게
+    client.to(roomId).emit('user_joined', {
+      socketId: client.id,
+      nickname: tokenResult.nickname,
+    });
+
+    const result = { connected: true, roomId };
+    return result;
   }
 }
