eaef9b284a54b45c0a0b4bf565b1fd71af807632
지토지토 <iam@zito.kr>
2026-02-24 23:29:04 +0900

    feat: 레디스 모듈 import

--- backend/src/game/game.module.ts
+++ backend/src/game/game.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { GameGateway } from './game.gateway';
 import { CustomJwtModule } from 'src/jwt/custom-jwt.module';
+import { RedisModule } from 'src/redis/redis.module';
 
 @Module({
-  imports: [CustomJwtModule],
+  imports: [CustomJwtModule, RedisModule],
   providers: [GameGateway],
 })
 export class GameModule {}
