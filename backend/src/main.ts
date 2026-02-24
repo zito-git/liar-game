20e1fc17e912847d5955f5be3fd2d83c31ef1400
지토지토 <iam@zito.kr>
2026-02-25 00:22:26 +0900

    feat: swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { GlobalExceptionFilter } from './filter/global.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new GlobalExceptionFilter());
+
+  const config = new DocumentBuilder()
+    .setTitle('Room API')
+    .setDescription('Room 관리 API 문서')
+    .setVersion('1.0')
+    .addBearerAuth() // JWT 인증 버튼 추가
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
